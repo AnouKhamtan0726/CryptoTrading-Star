f08bde4cfab96361d9bdf439e471efab86779f04
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-06-11 00:40:09 +0800

    deploy: https to http

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,5 +1,5 @@
 const DEV_MODE = false;
-const SERVER_URL = DEV_MODE ? "http://localhost:5000" : "https://206.189.147.229:5000";
+const SERVER_URL = DEV_MODE ? "http://localhost:5000" : "http://206.189.147.229:5000";
 
 module.exports = {
     DEV_MODE,
