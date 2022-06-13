df470bfbf76625c0d2b613c02de1a9f4acbdaf90
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-06-13 16:04:53 +0800

    deploy: to server

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("didi_db", "root", "", {
+const db = new Sequelize("didi_db", "root", "surprising99726", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
